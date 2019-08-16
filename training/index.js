c363107223c1ad033d6ab262ddbf817a5d53d0e0
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-16 15:33:02 +0200

    added store with reducer and connected index.js

--- training/index.js
+++ training/index.js
@@ -2,11 +2,15 @@ import { AppRegistry } from 'react-native';
 import App from './components/App';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './components/Store/reducers/counterReducer';
+
+const store = createStore(reducer);
 
 const AppContainer = () => 
-  // <Provider store={} >
-  <App />;
-  {/* </Provider> */}
+  <Provider store={store} >
+  <App />
+  </Provider>;
 
 
 AppRegistry.registerComponent('training', () => AppContainer);
